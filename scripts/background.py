27e50e653789c5f094b2a7c2c41d9c09ce131587
Antoine Boulanger <56837234+Antoine-Boulanger@users.noreply.github.com>
2021-06-11 22:13:52 -0400

    Dynamic tile loading and drawing from csv

--- scripts/background.py
+++ scripts/background.py
@@ -1,15 +1,14 @@
 import pygame
 
-TILE_SIZE = 16
+TILE_SIZE = 32
+TILESHEET_SIZE = (13, 8)
 TILESHEET_PATH = "./res/tiled/CosmicLilac_Tiles.png"
-
-FLOOR_TILE_POS = [1, 1]
-LEFT_WALL_TILE_POS = [0, 1]
-RIGHT_WALL_TILE_POS = [3, 1]
+CSV_PATH = "./res/tiled/testmap..csv"
 
 class Background():
     def __init__(self):
-        self.tileSheet = pygame.image.load(TILESHEET_PATH)
+        self.tileSheet = pygame.transform.scale(pygame.image.load(TILESHEET_PATH), (416, 256))
+        self.LoadTileCSV()
 
     def GetTile(self, posX, posY):
         rect = pygame.Rect(posX * TILE_SIZE, posY * TILE_SIZE, TILE_SIZE, TILE_SIZE)
@@ -17,17 +16,26 @@ class Background():
         image.blit(self.tileSheet, (0, 0), rect)
         return image
 
-    def DrawTile(screen, image, posX, posY):
-        screen.blit(image, (posX, posY))
+    def LoadTileCSV(self):
+        self.tileLayout = []
+        self.tileImages = {}
+        csvFile = open(CSV_PATH, 'r')
+
+        for line in csvFile:
+            currentRow = []
+            
+            for tileNum in line.split(','):
+                intTileNum = int(tileNum)
+                currentRow.append(intTileNum)
+
+                if (intTileNum not in self.tileImages):
+                    tilePosY = int(intTileNum / TILESHEET_SIZE[0])
+                    tilePosX = intTileNum - (tilePosY * TILESHEET_SIZE[0])
+                    self.tileImages.update({intTileNum: self.GetTile(tilePosX, tilePosY)})
+
+            self.tileLayout.append(currentRow)
 
     def Draw(self, screen):
-        for x in range(32):
-            for y in range(32):
-                if (x == 0):
-                    tile = self.GetTile(LEFT_WALL_TILE_POS[0], LEFT_WALL_TILE_POS[1])
-                elif (x == 31):
-                    tile = self.GetTile(RIGHT_WALL_TILE_POS[0], RIGHT_WALL_TILE_POS[1])
-                else:
-                    tile = self.GetTile(FLOOR_TILE_POS[0], FLOOR_TILE_POS[1])
-
-                screen.blit(tile, (x * TILE_SIZE, y * TILE_SIZE))
+        for y in range(len(self.tileLayout)):
+            for x in range(len(self.tileLayout[y])):
+                screen.blit(self.tileImages[self.tileLayout[y][x]], (x * TILE_SIZE, y * TILE_SIZE))
--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,7 @@
 import pygame
 from game import Game
 
-SIZE_X, SIZE_Y = 1000, 800
+SIZE_X, SIZE_Y = 1600, 900
 
 if (__name__ == "__main__"):
     screen = pygame.display.set_mode((SIZE_X, SIZE_Y))
