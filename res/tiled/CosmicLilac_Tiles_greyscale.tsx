0fdcbbe82de937ed1fdac357cc943902ff22df44
Nathan <nathan.gt.protic@gmail.com>
2021-06-12 01:21:33 -0400

    ajout textures sol

--- res/tiled/CosmicLilac_Tiles_greyscale.tsx
+++ res/tiled/CosmicLilac_Tiles_greyscale.tsx
@@ -1,4 +1,12 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.0" name="CosmicLilac_Tiles_greyscale" tilewidth="16" tileheight="16" tilecount="135" columns="15">
  <image source="CosmicLilac_Tiles_greyscale.png" width="240" height="144"/>
+ <wangsets>
+  <wangset name="Unnamed Set" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="13" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="28" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="43" wangid="0,1,0,1,0,1,0,1"/>
+  </wangset>
+ </wangsets>
 </tileset>
