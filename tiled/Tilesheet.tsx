1da097893748bc0cbb9c7aa48863c86bf4850623
Antoine Boulanger <56837234+Antoine-Boulanger@users.noreply.github.com>
2021-06-16 17:57:41 -0400

    Added fixed and optimised level 3 and cleaned files

--- tiled/Tilesheet.tsx
+++ tiled/Tilesheet.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="2021.03.23" name="Tilesheet" tilewidth="16" tileheight="16" tilecount="225" columns="15">
- <image source="../Tilesheet.png" width="240" height="240"/>
+ <image source="../res/Tilesheet.png" width="240" height="240"/>
 </tileset>
