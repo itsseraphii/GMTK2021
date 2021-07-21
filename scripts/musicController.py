8e957c97443a98a7af902b0e29b8f6eadf68ff3b
Psycho <56837234+psychopattt@users.noreply.github.com>
2021-07-21 14:18:06 -0400

    Level 7 V4 and music controller base volume

--- scripts/musicController.py
+++ scripts/musicController.py
@@ -2,6 +2,7 @@ from pygame.mixer import music as Music
 from enum import IntEnum
 from utils.constants import DATA_PATH
 
+BASE_VOLUME = 0
 MAIN_MUSIC_PATH = DATA_PATH + "/music/main.mp3"
 LEVEL_MUSIC_PATH = DATA_PATH + "/music/level.mp3"
 JINGLE_PATH = DATA_PATH + "/music/jingle.mp3"
@@ -16,36 +17,36 @@ class MusicEvents(IntEnum):
 
 def StartMusicMenu():
     Music.load(MAIN_MUSIC_PATH)
-    Music.set_volume(0.5)
+    Music.set_volume(BASE_VOLUME * 0.5)
     Music.play(-1) # Loop forever
 
 def StartMusicLevel():
     Music.load(LEVEL_MUSIC_PATH)
     Music.set_endevent(MusicEvents.START_JINGLE)
-    Music.set_volume(0.5)
+    Music.set_volume(BASE_VOLUME * 0.5)
     Music.play() # Play once
 
 def StartMusicJingle():
     Music.load(JINGLE_PATH)
     Music.set_endevent(MusicEvents.START_TIME_OVER)
-    Music.set_volume(0.5)
+    Music.set_volume(BASE_VOLUME * 0.5)
     Music.play()
 
 def StartMusicTimeOver():
     Music.load(TIME_OVER_MUSIC_PATH)
-    Music.set_volume(0.5)
+    Music.set_volume(BASE_VOLUME * 0.5)
     Music.play(-1)
 
 def StartMusicBoss():
     Music.load(BOSS_MUSIC_PATH)
     Music.set_endevent(MusicEvents.START_MENU)
-    Music.set_volume(0.7)
+    Music.set_volume(BASE_VOLUME * 0.7)
     Music.play()
 
 def StartMusicCredits():
     Music.load(CREDITS_MUSIC_PATH)
     Music.set_endevent(MusicEvents.START_MENU)
-    Music.set_volume(0.6)
+    Music.set_volume(BASE_VOLUME * 0.6)
     Music.play()
 
 def ProcessMusicEvents(event):
